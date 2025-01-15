1d159fcba618cc156a0b0c36b59814b7f6371201
arpadpall21 <a.pall21@yahoo.fr>
2025-01-15 08:25:52 +0100

    browser tab title translation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,23 @@
-import type { Metadata } from 'next';
 import { Red_Hat_Text } from 'next/font/google';
 import './globals.css';
 import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
-
-export const metadata: Metadata = {
-  title: 'Software Version History Timeline',
-  description: 'software version history timeline',
-};
+import { getMessages, getTranslations } from 'next-intl/server';
 
 const redHatText = Red_Hat_Text({ subsets: ['latin'] });
 
 async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   const messages = await getMessages();
+  const t = await getTranslations();
 
   return (
     <html lang="en">
+      <head>
+        <title>{t('mainTitle')}</title>
+        <meta
+          name="description"
+          content="Explore the Software Version History Timeline to compare key updates and milestones across different software releases."
+        />
+      </head>
       <NextIntlClientProvider messages={messages}>
         <body className={`${redHatText.className} antialiased`}>{children}</body>
       </NextIntlClientProvider>
