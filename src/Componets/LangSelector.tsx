bd09886e3397aa2d3eae7f722a4046ed43699931
arpadpall21 <a.pall21@yahoo.fr>
2025-01-18 19:45:35 +0100

    language selector working finally :P

--- src/Componets/LangSelector.tsx
+++ src/Componets/LangSelector.tsx
@@ -1,23 +1,27 @@
 'use client';
 
-import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
 import { validLang } from '@/misc/helpers';
+import Cookies from 'js-cookie';
 import { type Lang } from '@/misc/types';
 import appSettings from '@/misc/appSettings';
 
+const supportedLanguages = appSettings.lang.supportedLanguages;
+
 const LangSelector: React.FC = () => {
+  const router = useRouter();
   const lang: Lang = validLang(Cookies.get('lang'));
 
   function dropdownHandler(e: React.ChangeEvent<HTMLSelectElement>) {
     Cookies.set('lang', e.target.value);
-    window.location.reload();
+    router.refresh();
   }
 
   return (
     <select value={lang.langCode} onChange={dropdownHandler}>
-      {Object.keys(appSettings.lang.supportedLanguages).map((lang) => (
-        <option value={appSettings.lang.supportedLanguages[lang].langCode} key={lang}>
-          {appSettings.lang.supportedLanguages[lang].lang}
+      {Object.keys(supportedLanguages).map((lang) => (
+        <option value={supportedLanguages[lang].langCode} key={lang}>
+          {supportedLanguages[lang].lang}
         </option>
       ))}
     </select>
