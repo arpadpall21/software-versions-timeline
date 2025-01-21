f2cecc2e275e78b7431c97f038fbaaec764b7ab4
arpadpall21 <a.pall21@yahoo.fr>
2025-01-21 20:30:44 +0100

    done

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -7,13 +7,16 @@ const Footer: React.FC = async () => {
   const t = await getTranslations('components.footer');
 
   return (
-    <footer className={'flex justify-between p-7 mb-4 border-t-2 border-borPri dark:border-borPriD'}>
+    <footer
+      className={`flex flex-col sm:flex-row justify-between py-7 sm:px-7 mb-4
+        border-t-2 border-borPri dark:border-borPriD`}
+    >
       <div>
         <p className={'text-fgSec dark:text-fgSecD'}>{t('copyright', { name: 'Arpad Pall' })}</p>
         <p className={'pt-3 text-fgSec dark:text-fgSecD'}> Email: a.pall21@yahoo.fr </p>
       </div>
-      <div className={'flex'}>
-        <div className={'inline-block mr-4'}>
+      <div className={'w-36 mt-6 sm:mt-0 md:w-auto'}>
+        <div className={'inline-block mr-4 mb-2'}>
           <ThemeSelector />
         </div>
         <div className={'inline-block'}>
