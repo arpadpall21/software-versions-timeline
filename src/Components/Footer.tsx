d30efb058a101c99466bf631f41fe18f3ca7d86d
arpadpall21 <a.pall21@yahoo.fr>
2025-01-21 16:33:41 +0100

    colors ok

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,16 +4,16 @@ import LangSelector from './LangSelector';
 
 const Footer: React.FC = () => {
   return (
-    <footer className={'flex justify-between p-7 mb-6 border-t-2 border-borPri dark:border-borPriD'}>
+    <footer className={'flex justify-between p-7 mb-4 border-t-2 border-borPri dark:border-borPriD'}>
       <div>
-        <p> Copyright © 2025 Arpad Pall. All rights reserved. </p>
-        <p className={'pt-3'}> Email: a.pall21@yahoo.fr </p>
+        <p className={'text-fgSec dark:text-fgSecD'}> Copyright © 2025 Arpad Pall. All rights reserved. </p>
+        <p className={'pt-3 text-fgSec dark:text-fgSecD'}> Email: a.pall21@yahoo.fr </p>
       </div>
       <div>
-        <div className={'inline-block mr-4'}>
+        <div className={'inline-block'}>
           <ThemeSelector />
         </div>
-        <div className={'inline-block'}>
+        <div className={'inline-block ml-4'}>
           <LangSelector />
         </div>
       </div>
