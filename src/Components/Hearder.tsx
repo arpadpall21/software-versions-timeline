dc10c281136f526ae2c3c4227c7950edae82999d
arpadpall21 <a.pall21@yahoo.fr>
2025-01-22 18:32:05 +0100

    done

--- src/Components/Hearder.tsx
+++ src/Components/Hearder.tsx
@@ -7,17 +7,10 @@ const Header: React.FC = () => {
 
   return (
     <header className={'border-b-2 border-borPri dark:border-borPriD'}>
-      <h1 className={'text-2xl font-bold my-2 mx-mobPad sm:mx-desckPad'}>
+      <h1 className={'text-lg sm:text-2xl font-bold my-2 mx-mobPad sm:mx-desckPad'}>
         <Image className={'inline mr-2'} src={logo} width={28} height={28} alt="Logo" />
         <p className={'inline align-middle'}>{t('mainTitle')}</p>
       </h1>
-      
-      
-      {/* <div className={'flex justify-between my-3'}>
-        <div> left section </div>
-        <h1 className={'text-2xl font-bold'}>{t('mainTitle')}</h1>
-        <div> </div>
-      </div> */}
     </header>
   );
 };
--- src/app/version-map/page.tsx
+++ src/app/version-map/page.tsx
@@ -3,26 +3,31 @@ import '@/app/globals.css';
 const Home = () => {
   return (
     <>
-      <p className={'text-fgPri dark:text-fgPriD'}> Hello World! </p>
-      <p className={'text-fgPri dark:text-fgPriD text-lg font-bold'}> Hello World! </p>
-      <p className={'text-fgPri dark:text-fgPriD text-xl'}> Hello World! </p>
-      <p className={'text-fgPri dark:text-fgPriD text-2xl font-bold'}> Hello World! </p>
-      
-      <p className={'text-fgSec dark:text-fgSecD'}> Hello World! </p>
-      <p className={'text-fgSec dark:text-fgSecD text-lg font-bold'}> Hello World! </p>
-      <p className={'text-fgSec dark:text-fgSecD text-xl'}> Hello World! </p>
-      <p className={'text-fgSec dark:text-fgSecD text-2xl font-bold'}> Hello World! </p>
-      
-      <p className={'text-fgPop dark:text-fgPopD'}> Pop color </p>
-      <p className={'text-fgPop dark:text-fgPopD text-lg font-bold'}> Pop color </p>
-      <p className={'text-fgPop dark:text-fgPopD text-xl'}> Pop color </p>
-      <p className={'text-fgPop dark:text-fgPopD text-2xl font-bold'}> Pop color </p>
-      
-      
-      <br /><br /><br /><br /><br /><br /><br /> 
-      <br /><br /><br /><br /><br /><br /><br /> 
-      <br /><br /><br /><br /><br /><br /><br /> 
-      
+      <p> Hello World!</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </>
   );
 };
