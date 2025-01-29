61cb5052a37b3aff899a6d45ad4d560f789790a1
arpadpall21 <a.pall21@yahoo.fr>
2025-01-29 20:43:22 +0100

    basic timeline grid

---
+++ src/app/version-map/Components/TimelineGrid.tsx
@@ -0,0 +1,13 @@
+const nrOfMonths = 24;
+
+const TimelineGrid: React.FC = () => {
+  return (
+    <div className={'h-[100px] flex bg-blue-50'}>
+      {Array.from({ length: nrOfMonths }, (_, i) => (
+        <div key={i} className={'border-l border-b border-borPri h-full w-[30px]'}></div>
+      ))}
+    </div>
+  );
+};
+
+export default TimelineGrid;
