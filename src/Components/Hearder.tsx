51e02e16f41f31c2d4dae797e25561dc486d0d29
arpadpall21 <a.pall21@yahoo.fr>
2025-04-15 20:50:52 +0200

    larger main logo

--- src/Components/Hearder.tsx
+++ src/Components/Hearder.tsx
@@ -8,7 +8,7 @@ const Header: React.FC = async () => {
   return (
     <header className={'border-b-2 border-borPri dark:border-borPriD'}>
       <h1 className={'text-lg sm:text-2xl font-bold my-3 mx-mobPad sm:mx-deskPad'}>
-        <Image className={'inline mr-2'} src={logo} width={28} height={28} alt="Logo" priority={false} />
+        <Image className={'inline mr-2'} src={logo} width={40} height={40} alt="Logo" priority={false} />
         <p className={'inline align-middle'}>{t('mainTitle')}</p>
       </h1>
     </header>
