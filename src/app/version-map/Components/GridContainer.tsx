0fb75efc5dddd52c927a0cfbfddb5c817ec90edb
arpadpall21 <a.pall21@yahoo.fr>
2025-05-02 18:46:12 +0200

    grid container component

---
+++ src/app/version-map/Components/GridContainer.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import GridFrame from '@/app/version-map/Components/GridFrame';
+
+const GridContainer: React.FC = () => {
+  return <GridFrame />;
+};
+
+export default GridContainer;
