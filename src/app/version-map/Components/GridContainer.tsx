9a5d9482f8571df72e8c5c1ca7dd3aeb21ed00a7
arpadpall21 <a.pall21@yahoo.fr>
2025-05-03 11:52:54 +0200

    carousel component removed

--- src/app/version-map/Components/GridContainer.tsx
+++ src/app/version-map/Components/GridContainer.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react';
 import GridFrame from '@/app/version-map/Components/GridFrame';
 import Button from '@/Components/Button';
-import Carousel from '@/Components/Carousel';
 import { calcMonthsUpToCurrent } from '@/misc/helpers';
 import { type Month } from '@/misc/types';
 
@@ -11,18 +10,18 @@ const arr = [
   1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996,
   1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
   2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020,
-  2021, 2022, 2023, 2024, 2025
+  2021, 2022, 2023, 2024, 2025, 'Latest'
 ]
+arr.reverse()
 
 const GridContainer: React.FC = () => {
   const [displayedMonths, setdisplayedMonths] = useState<Month[]>(calcMonthsUpToCurrent(2023, 6));
 
   return (
     <>
-      <Carousel>
+      <div className={'my-7 overflow-hidden whitespace-nowrap'} style={{ direction: 'rtl' }}>
         {arr.map((i) => <Button text={i.toString()} width={86} key={i}/> )}
-        <Button text={'Latest'} width={110} pop={true}/>
-      </Carousel>
+      </div>
       <GridFrame displayedMonths={displayedMonths} setDisplayedMonths={setdisplayedMonths} />
     </>
   );
