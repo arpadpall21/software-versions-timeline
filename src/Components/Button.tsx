061c94c797172374143b3135b858af6d648ad1c2
arpadpall21 <a.pall21@yahoo.fr>
2025-05-03 06:49:14 +0200

    button ok

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,11 +1,26 @@
 'use client';
 
 interface Props {
-  handleClick?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  text: string;
+  pop?: boolean;
+  handleClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button: React.FC<Props> = ({ handleClick }) => {
-  return <p> Button </p>;
+const Button: React.FC<Props> = ({ text, pop, handleClick }) => {
+  return (
+    <button
+      className={` shadow-md px-3 ml-1
+        border-2 border-borPri dark:border-borPriD rounded-md bg-bgPri dark:bg-bgPriD
+        hover:bg-bgIntHover dark:hover:bg-bgIntHoverD`}
+      onClick={handleClick}
+    >
+      {pop ? (
+        <span className={'text-fgPri dark:text-fgPriD font-semibold'}>{text}</span>
+      ) : (
+        <span className={'text-fgPop dark:text-fgPopD'}>{text}</span>
+      )}
+    </button>
+  );
 };
 
 export default Button;
--- src/app/version-map/Components/GridContainer.tsx
+++ src/app/version-map/Components/GridContainer.tsx
@@ -2,13 +2,23 @@
 
 import { useState } from 'react';
 import GridFrame from '@/app/version-map/Components/GridFrame';
+import Button from '@/Components/Button';
 import { calcMonthsUpToCurrent } from '@/misc/helpers';
 import { type Month } from '@/misc/types';
 
 const GridContainer: React.FC = () => {
   const [displayedMonths, setdisplayedMonths] = useState<Month[]>(calcMonthsUpToCurrent(2023, 6));
 
-  return <GridFrame displayedMonths={displayedMonths} setDisplayedMonths={setdisplayedMonths} />;
+  return (
+    <>
+      <div className={'my-7'}>
+        <Button text={'2024'} />
+        <Button text={'2025'} />
+        <Button text={'Latest'} pop={true}/>
+      </div>
+      <GridFrame displayedMonths={displayedMonths} setDisplayedMonths={setdisplayedMonths} />
+    </>
+  );
 };
 
 export default GridContainer;
