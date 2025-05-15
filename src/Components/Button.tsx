d70f6afef5c0c5504fb2d4c38c333b394dd9f7e9
Páll Árpád <a.pall21@yahoo.fr>
2025-05-15 11:25:37 +0200

    Remove button component tailwind code duplication
    * button component tailwind style logic refactored
    
    * button component tailwind style comments removed

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -8,34 +8,16 @@ interface Props {
 }
 
 const Button: React.FC<Props> = ({ text, width, pop, handleClick }) => {
-  /**
-   * Tailwind utilities are parsed at build time so they cannot be iterpolated with values,
-   *   in this situation this means we cannot conditionally change tailwind utilities
-   * So code duplication is kind of unavoidable here :/
-   *
-   * https://tailwindcss.com/docs/detecting-classes-in-source-files#class-detection-in-depth
-   */
+  let twStyle = `bg-btnBg dark:bg-btnBgD text-btnFg dark:text-btnFgD
+      hover:bg-btnBgHov dark:hover:bg-btnBgHovD`;
   if (pop) {
-    return (
-      <button
-        className={`shadow-md px-3 ml-1 
-          border-2 border-borPri dark:border-borPriD rounded-md bg-btnBgPop dark:bg-btnBgPopD
-          font-bold text-btnFgPop dark:text-btnFgPopD 
-          hover:bg-btnBgHovPop dark:hover:bg-btnBgHovPopD`}
-        style={{ width }}
-        onClick={handleClick}
-      >
-        {text}
-      </button>
-    );
+    twStyle = `bg-btnBgPop dark:bg-btnBgPopD font-bold text-btnFgPop
+      dark:text-btnFgPopD hover:bg-btnBgHovPop dark:hover:bg-btnBgHovPopD`;
   }
 
   return (
     <button
-      className={` shadow-md px-3 ml-1
-        border-2 border-borPri dark:border-borPriD rounded-md bg-btnBg dark:bg-btnBgD
-        text-btnFg dark:text-btnFgD 
-        hover:bg-btnBgHov dark:hover:bg-btnBgHovD`}
+      className={`shadow-md px-3 ml-1 border-2 border-borPri dark:border-borPriD rounded-md ${twStyle}`}
       style={{ width }}
       onClick={handleClick}
     >
