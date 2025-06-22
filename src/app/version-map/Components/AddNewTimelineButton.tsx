d643ad59943b88ed8c99d5121b29a6d74fde0da0
arpadpall21 <a.pall21@yahoo.fr>
2025-06-22 08:41:36 +0200

    dropdown added and hidden

--- src/app/version-map/Components/AddNewTimelineButton.tsx
+++ src/app/version-map/Components/AddNewTimelineButton.tsx
@@ -1,11 +1,25 @@
+import { useContext } from 'react';
+import { GridContainerContext } from '@/app/version-map/Components/GridContainer';
+import appConfig from '../../../../config/appConfig';
+
 interface Props {
   height: number;
 }
 
 const AddNewTimelineButton: React.FC<Props> = ({ height }) => {
+  const {  } = useContext(GridContainerContext);
+
   return (
-    <div className={'bg-red-300'} style={{ height }} />
-  )
-}
+    <div className={'bg-red-300'} style={{ height }}>
+      <select className={'hidden'}>
+        {Object.entries(appConfig.supportedSoftwares).map(([software, supportedSoftware], i) => (
+          <option value={software} key={i}>
+            {supportedSoftware.displayName}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
 export default AddNewTimelineButton;
