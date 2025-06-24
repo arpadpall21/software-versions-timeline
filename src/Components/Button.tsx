21673b40d236dc9506afd5b51fc5485c1b2765cc
arpadpall21 <a.pall21@yahoo.fr>
2025-06-24 07:48:06 +0200

    focus ring globalized

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -10,7 +10,7 @@ interface Props {
 const Button: React.FC<Props> = ({ text, width, pop, handleClick }) => {
   return (
     <button
-      className={`${pop ? 'btnPop dark:btnPopD' : 'btn dark:btnD'} px-3 ml-1 `}
+      className={`${pop ? 'btnPop dark:btnPopD' : 'btn dark:btnD'} px-3 ml-1`}
       style={{ width }}
       onClick={handleClick}
     >
@@ -20,3 +20,6 @@ const Button: React.FC<Props> = ({ text, width, pop, handleClick }) => {
 };
 
 export default Button;
+
+
+// outline-2 outline-foc dark:outline-focD sm:has-[:focus]:outline`
