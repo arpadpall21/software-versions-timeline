d529f6e2f451e27972377689d143de1fd77202d1
arpadpall21 <a.pall21@yahoo.fr>
2025-06-26 07:32:55 +0200

    commit before slider logic implementation

--- src/Components/HorizontalScroll.tsx
+++ src/Components/HorizontalScroll.tsx
@@ -15,15 +15,22 @@ const HorizontalScroll: React.FC<Props> = ({
   scrollRightButton,
   direction = 'ltr',
 }) => {
+  const sliderParentStart = direction === 'ltr' ? 'left-0' : 'right-0';
+
   return (
     <div className={`relative whitespace-nowrap overflow-hidden ${twStyle}`}>
       <div className={'absolute left-0 z-10'}>{scrollLeftButton}</div>
       <div className={'absolute right-0 z-10'}>{scrollRightButton}</div>
-      <div className={'absolute right-0'} style={{ direction }}>
-        {direction === 'rtl' ? [...members].reverse() : members}
+      <div className={`absolute ${sliderParentStart}`} style={{ direction }}>
+        <div className={'border-2'}>{direction === 'rtl' ? [...members].reverse() : members}</div>
       </div>
     </div>
   );
 };
 
 export default HorizontalScroll;
+
+
+//  const parent = child.parentElement;
+//     const minX = 0; // Minimum X position (left edge of parent)
+//     const maxX = parent.clientWidth - child.clientWidth; 
