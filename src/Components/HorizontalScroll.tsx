50582e560983fa6948cfe80c0ce589ffec759596
arpadpall21 <a.pall21@yahoo.fr>
2025-06-29 19:26:54 +0200

    simplifying scroll handler + direction buttons positioning

--- src/Components/HorizontalScroll.tsx
+++ src/Components/HorizontalScroll.tsx
@@ -25,30 +25,32 @@ const HorizontalScroll: React.FC<Props> = ({
     return start === 'left' ? members : [...members].reverse();
   }, [members, start]);
 
-  function handleScrollLeft() {
+  function handleScroll(direction: 'left' | 'right') {
     if (sliderRef.current) {
-      sliderRef.current.scrollBy({ left: -scrollSensitivity, behavior: 'smooth' });
-    }
-  }
-
-  function handleScrollRight() {
-    if (sliderRef.current) {
-      sliderRef.current.scrollBy({ left: scrollSensitivity, behavior: 'smooth' });
+      sliderRef.current.scrollBy({
+        left: direction === 'left' ? -scrollSensitivity : scrollSensitivity,
+        behavior: 'smooth',
+      });
     }
   }
 
   return (
     <>
-      <div onMouseDown={handleScrollLeft}>{scrollLeftButton}</div>
-      <div onMouseDown={handleScrollRight}>{scrollRightButton}</div>
-
-      <div className={`w-full overflow-hidden`} style={{ height, direction: start === 'left' ? 'ltr' : 'rtl' }}>
-        <div
-          className={'overflow-x-scroll overflow-y-hidden whitespace-nowrap h-full'}
-          style={{ paddingBottom: height + 50 }}
-          ref={sliderRef}
-        >
-          {renderedMembers}
+      <div className={`relative w-full overflow-hidden`} style={{ height }}>
+        <div className={'absolute left-0'} onMouseDown={() => handleScroll('left')}>
+          {scrollLeftButton}
+        </div>
+        <div className={'absolute right-0'} onMouseDown={() => handleScroll('right')}>
+          {scrollRightButton}
+        </div>
+        <div>
+          <div
+            className={'overflow-x-scroll overflow-y-hidden whitespace-nowrap h-full'}
+            style={{ paddingBottom: height + 50, direction: start === 'left' ? 'ltr' : 'rtl' }}
+            ref={sliderRef}
+          >
+            {renderedMembers}
+          </div>
         </div>
       </div>
     </>
