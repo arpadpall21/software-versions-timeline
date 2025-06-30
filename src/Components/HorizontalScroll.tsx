cd068a9c8b9f27731c1e705248eb6cdd0107dfd3
arpadpall21 <a.pall21@yahoo.fr>
2025-06-30 08:17:11 +0200

    scroll button visibility logic + scroll distance calc

--- src/Components/HorizontalScroll.tsx
+++ src/Components/HorizontalScroll.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useMemo, RefObject } from 'react';
+import { useState, useRef, useMemo, type RefObject } from 'react';
 
 interface Props {
   height: number;
@@ -19,31 +19,63 @@ const HorizontalScroll: React.FC<Props> = ({
   scrollSensitivity = 100,
   start = 'left',
 }) => {
+  const [sliderPosition, setSliderPosition] = useState<number>(0);
+  const [leftScrollButtonVisible, setLeftScrollButtonVisible] = useState<boolean>(false);
+  const [rightScrollButtonVisible, setRightScrollButtonVisible] = useState<boolean>(false);
+
   const sliderRef: RefObject<null | HTMLDivElement> = useRef(null);
+  const sliderChildRef: RefObject<null | HTMLDivElement> = useRef(null);
 
   const renderedMembers: React.ReactNode[] = useMemo(() => {
     return start === 'left' ? members : [...members].reverse();
   }, [members, start]);
 
   function handleScroll(direction: 'left' | 'right') {
-    if (sliderRef.current) {
-      sliderRef.current.scrollBy({
-        left: direction === 'left' ? -scrollSensitivity : scrollSensitivity,
+    if (sliderRef.current && sliderChildRef.current) {
+      const scrollableDistance: number = sliderChildRef.current.offsetWidth - sliderRef.current.offsetWidth;
+      
+      
+      const scrollBy: number =
+        direction === 'left' ? sliderPosition + scrollSensitivity : sliderPosition - scrollSensitivity;
+      
+      
+      
+      
+      console.log(scrollBy)
+      console.log(scrollableDistance)
+      
+      
+      sliderRef.current.scrollTo({
+        left: scrollBy,
         behavior: 'smooth',
       });
+      
+      setSliderPosition(scrollBy);
+      
+      
+      // sliderRef.current.scrollBy({
+      //   left: direction === 'left' ? -scrollSensitivity : scrollSensitivity,
+      //   behavior: 'smooth',
+      // });
     }
   }
 
   return (
     <>
       <div className={`relative w-full overflow-hidden`} style={{ height }}>
-        <div className={'absolute left-0 h-full'}>
+        <div
+          className={'absolute left-0 h-full'}
+          style={{ visibility: leftScrollButtonVisible ? 'visible' : 'hidden' }}
+        >
           <div className={'float-left h-full bg-bgPri dark:bg-bgPriD'} onMouseDown={() => handleScroll('left')}>
             {scrollLeftButton}
           </div>
           <div className={'float-left w-[20px] h-full bg-grl dark:bg-grlD'} />
         </div>
-        <div className={'absolute right-0 h-full'}>
+        <div
+          className={'absolute right-0 h-full'}
+          style={{ visibility: rightScrollButtonVisible ? 'visible' : 'hidden' }}
+        >
           <div className={'float-right h-full bg-bgPri dark:bg-bgPriD'} onMouseDown={() => handleScroll('right')}>
             {scrollRightButton}
           </div>
@@ -51,11 +83,13 @@ const HorizontalScroll: React.FC<Props> = ({
         </div>
         <div>
           <div
-            className={'overflow-x-scroll overflow-y-hidden whitespace-nowrap h-full'}
+            className={'overflow-x-scroll whitespace-nowrap h-full'}
             style={{ paddingBottom: height + 50, direction: start === 'left' ? 'ltr' : 'rtl' }}
             ref={sliderRef}
           >
-            {renderedMembers}
+            <div className={'inline-block'} ref={sliderChildRef}>
+              {renderedMembers}
+            </div>
           </div>
         </div>
       </div>
