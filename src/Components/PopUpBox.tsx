c7ce0532c455cd8c37e31a1ad93ab63e5211c64e
arpadpall21 <a.pall21@yahoo.fr>
2025-07-03 07:07:24 +0200

    pop up animation

--- src/Components/PopUpBox.tsx
+++ src/Components/PopUpBox.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 interface Props {
   message: string;
   timeout: number;
@@ -5,11 +7,27 @@ interface Props {
 }
 
 const PopUpBox: React.FC<Props> = ({ message, timeout, dialog = false }) => {
+  const [active, setActive] = useState(true);
+
+  // useEffect(() => {
+  //   setTimeout(() => setActive(!active), 3000);
+  // }, [active])
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
   return (
     <div
-      className={`fixed top-5 left-[50%] translate-x-[-50%] max-w-[640px]
+      className={`fixed left-[50%] translate-x-[-50%] max-w-[640px]
         text-justify py-[25px] px-[35px] z-50 shadow-2xl rounded-md
         bg-bgPri dark:bg-bgPriD border-2 border-borPopUp dark:border-PopUpD`}
+      style={{
+        top: active ? 30 : -500,
+        visibility: active ? 'visible' : 'hidden',
+        transition: 'top 0.5s ease-in, visibility 0.5s ease-in',
+      }}
+      onClick={handleClick}
     >
       <p>{message}</p>
     </div>
