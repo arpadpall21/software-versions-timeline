b4569113a202d687aad29fb4537b5a1670e1f666
arpadpall21 <a.pall21@yahoo.fr>
2025-07-16 18:46:25 +0300

    intro text added (en language)

--- src/app/version-map/page.tsx
+++ src/app/version-map/page.tsx
@@ -1,14 +1,18 @@
 import '@/app/globals.css';
 import GridContainer from '@/app/version-map/Components/GridContainer';
 import SupportedSoftwares from '@/app/version-map/Components/SupportedSoftwares';
+import { getTranslations } from 'next-intl/server';
 
-const VersionMapPage = () => {
+const VersionMapPage = async () => {
+  const t = await getTranslations();
+
   return (
     <>
       <GridContainer />
       <div className={'max-sm:hidden md:block my-7'}>
         <SupportedSoftwares />
       </div>
+      <p className={'text-justify max-sm:hidden my-7'}> {t('introText')} </p>
     </>
   );
 };
