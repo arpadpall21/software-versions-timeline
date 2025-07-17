2e43948b0d4ef00aa46ba144133836ec782f9a80
arpadpall21 <a.pall21@yahoo.fr>
2025-07-17 20:01:13 +0300

    bug fixed

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -11,15 +11,15 @@ const Footer: React.FC = async () => {
       className={`flex sm:flex-row flex-col justify-between py-7 mx-mobPad sm:mx-deskPad sm:px-7 mb-4
         border-t-2 border-borPri dark:border-borPriD`}
     >
-      <div>
+      <div className={'mr-2'}>
         <p className={'text-fgSec dark:text-fgSecD'}>{t('copyright', { name: 'Arpad Pall' })}</p>
         <p className={'pt-3 text-fgSec dark:text-fgSecD'}> Email: a.pall21@yahoo.fr </p>
       </div>
-      <div className={'mt-6 sm:mt-0 sm:w-auto'}>
-        <div className={'sm:inline-block block mr-4 mb-2'}>
+      <div className={'mt-6 sm:mt-0'}>
+        <div className={'block md:inline-block mr-4 mb-2'}>
           <ThemeSelector />
         </div>
-        <div className={'sm:inline-block block'}>
+        <div className={'block md:inline-block'}>
           <LangSelector />
         </div>
       </div>
