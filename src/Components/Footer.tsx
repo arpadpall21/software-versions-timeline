7da1039ecd78112ce76984ae27456b7182baa492
Páll Árpád <a.pall21@yahoo.fr>
2025-07-17 20:03:12 +0300

    Mobile Adaptation - Footer Bug Fix
    * right box width tackled
    
    * bug fixed

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -8,18 +8,18 @@ const Footer: React.FC = async () => {
 
   return (
     <footer
-      className={`flex flex-col sm:flex-row justify-between py-7 mx-mobPad sm:mx-deskPad sm:px-7 mb-4
+      className={`flex sm:flex-row flex-col justify-between py-7 mx-mobPad sm:mx-deskPad sm:px-7 mb-4
         border-t-2 border-borPri dark:border-borPriD`}
     >
-      <div>
+      <div className={'mr-2'}>
         <p className={'text-fgSec dark:text-fgSecD'}>{t('copyright', { name: 'Arpad Pall' })}</p>
         <p className={'pt-3 text-fgSec dark:text-fgSecD'}> Email: a.pall21@yahoo.fr </p>
       </div>
-      <div className={'w-36 mt-6 sm:mt-0 md:w-auto'}>
-        <div className={'inline-block mr-4 mb-2'}>
+      <div className={'mt-6 sm:mt-0'}>
+        <div className={'block md:inline-block mr-4 mb-2'}>
           <ThemeSelector />
         </div>
-        <div className={'inline-block'}>
+        <div className={'block md:inline-block'}>
           <LangSelector />
         </div>
       </div>
