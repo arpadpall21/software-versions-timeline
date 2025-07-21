ced7cab9b589247178fd8fa39a6f037de5c72197
Arpad <arpad.pall@zeiss.com>
2025-07-21 18:51:03 +0200

    side logo dropdown top + display only on default zoom level

--- src/app/version-map/Components/SideLogo.tsx
+++ src/app/version-map/Components/SideLogo.tsx
@@ -26,12 +26,10 @@ const SideLogo: React.FC<Props> = ({ twStyle, software, idx }) => {
   const tSideLogo = useTranslations('components.sideLogo');
   const tPopUpBox = useTranslations('components.popUpBox.messages');
 
-  const { scaleLogoX, scaleLogoY, scaleDropdownY, bottomSpaceDropdown } = useMemo(() => {
+  const { scaleLogoX, scaleLogoY } = useMemo(() => {
     return {
       scaleLogoX: zoomLevel < defaultZoomLevel ? zoomLevel : defaultZoomLevel,
       scaleLogoY: zoomLevel <= defaultZoomLevel ? defaultZoomLevel : calcPercentOf(defaultZoomLevel, zoomLevel) / 100,
-      scaleDropdownY: calcPercentOf(defaultZoomLevel, zoomLevel) / 100,
-      bottomSpaceDropdown: zoomLevel >= defaultZoomLevel ? 2 * zoomLevel : 6 / zoomLevel,
     };
   }, [zoomLevel]);
 
@@ -59,21 +57,22 @@ const SideLogo: React.FC<Props> = ({ twStyle, software, idx }) => {
   return (
     <div className={`flex h-[100px] ${twStyle}`}>
       <div className={'relative w-[70px] h-[70px] m-auto'}>
-        <select
-          className={`btn dark:btnD absolute w-[19px] right-[5px] z-10
-            hover:cursor-pointer`}
-          style={{ bottom: bottomSpaceDropdown, transform: `scaleY(${scaleDropdownY})` }}
-          value={software}
-          title={tSideLogo('dropDownTooltip')}
-          onChange={handleDropdown}
-        >
-          <option value={'removeTimeline'}>{`[${tSideLogo('removeTimeline')}]`}</option>
-          {Object.entries(appConfig.supportedSoftwares).map(([software, supportedSoftware], i) => (
-            <option value={software} key={i}>
-              {supportedSoftware.displayName}
-            </option>
-          ))}
-        </select>
+        {zoomLevel === defaultZoomLevel && (
+          <select
+            className={`btn dark:btnD absolute w-[19px] h-[30px] right-[5px] top-0 z-10
+              hover:cursor-pointer`}
+            value={software}
+            title={tSideLogo('dropDownTooltip')}
+            onChange={handleDropdown}
+          >
+            <option value={'removeTimeline'}>{`[${tSideLogo('removeTimeline')}]`}</option>
+            {Object.entries(appConfig.supportedSoftwares).map(([software, supportedSoftware], i) => (
+              <option value={software} key={i}>
+                {supportedSoftware.displayName}
+              </option>
+            ))}
+          </select>
+        )}
         <div
           className={'absolute bottom-1 right-1 smoothTransform'}
           style={{ transform: `scaleX(${scaleLogoX}) scaleY(${scaleLogoY})` }}
