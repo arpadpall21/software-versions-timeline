0a0ccf013cb2be1d025fbcdf2f505dc7238da9c2
Páll Árpád <a.pall21@yahoo.fr>
2025-07-26 11:13:37 +0200

    Refactored Data Files to JS Objects
    * readme update
    
    * chrome data file formated
    
    * action refactored to support dynamic import
    
    * edge: data file refactored
    
    * internet explorer: data file refactored
    
    * mozilla: data file refactored
    
    * nodeJs: data file refactored
    
    * opera: data file refactored
    
    * python: data file refactored
    
    * reactJs: data file refactored
    
    * safar: data file refactored

---
+++ data/python.ts
@@ -0,0 +1,182 @@
+import { type RawHistoryData } from '@/misc/types';
+
+const data: RawHistoryData = {
+  '2001-06': {
+    versions: [
+      {
+        day: 22,
+        version: '2.0',
+      },
+    ],
+  },
+  '2001-12': {
+    versions: [
+      {
+        day: 21,
+        version: '2.2',
+      },
+    ],
+  },
+  '2002-04': {
+    versions: [
+      {
+        day: 9,
+        version: '2.1',
+      },
+    ],
+  },
+  '2003-07': {
+    versions: [
+      {
+        day: 29,
+        version: '2.3',
+      },
+    ],
+  },
+  '2004-11': {
+    versions: [
+      {
+        day: 30,
+        version: '2.4',
+      },
+    ],
+  },
+  '2006-09': {
+    versions: [
+      {
+        day: 19,
+        version: '2.5',
+      },
+    ],
+  },
+  '2008-10': {
+    versions: [
+      {
+        day: 2,
+        version: '2.6',
+      },
+    ],
+  },
+  '2008-12': {
+    versions: [
+      {
+        day: 3,
+        version: '3',
+      },
+    ],
+  },
+  '2009-06': {
+    versions: [
+      {
+        day: 26,
+        version: '3.1',
+      },
+    ],
+  },
+  '2010-07': {
+    versions: [
+      {
+        day: 3,
+        version: '2.7',
+      },
+    ],
+  },
+  '2011-02': {
+    versions: [
+      {
+        day: 20,
+        version: '3.2',
+      },
+    ],
+  },
+  '2012-09': {
+    versions: [
+      {
+        day: 29,
+        version: '3.3',
+      },
+    ],
+  },
+  '2014-03': {
+    versions: [
+      {
+        day: 17,
+        version: '3.4',
+      },
+    ],
+  },
+  '2015-09': {
+    versions: [
+      {
+        day: 13,
+        version: '3.5',
+      },
+    ],
+  },
+  '2016-12': {
+    versions: [
+      {
+        day: 23,
+        version: '3.6',
+      },
+    ],
+  },
+  '2018-06': {
+    versions: [
+      {
+        day: 27,
+        version: '3.7',
+      },
+    ],
+  },
+  '2019-08': {
+    versions: [
+      {
+        day: 14,
+        version: '3.8',
+      },
+    ],
+  },
+  '2020-08': {
+    versions: [
+      {
+        day: 5,
+        version: '3.9',
+      },
+    ],
+  },
+  '2021-08': {
+    versions: [
+      {
+        day: 4,
+        version: '3.10',
+      },
+    ],
+  },
+  '2022-08': {
+    versions: [
+      {
+        day: 24,
+        version: '3.11',
+      },
+    ],
+  },
+  '2023-08': {
+    versions: [
+      {
+        day: 2,
+        version: '3.12',
+      },
+    ],
+  },
+  '2024-10': {
+    versions: [
+      {
+        day: 7,
+        version: '3.13',
+      },
+    ],
+  },
+};
+
+export default data;
