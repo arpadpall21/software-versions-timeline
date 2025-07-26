a2869d870b65d2db1fb23bef5ccadf8de5381726
arpadpall21 <a.pall21@yahoo.fr>
2025-07-26 10:46:13 +0200

    internet explorer: data file refactored

---
+++ data/internetExplorer.ts
@@ -0,0 +1,94 @@
+import { type RawHistoryData } from '@/misc/types';
+
+const data: RawHistoryData = {
+  '1995-08': {
+    versions: [
+      {
+        day: 24,
+        version: '1',
+      },
+    ],
+  },
+  '1995-11': {
+    versions: [
+      {
+        day: 22,
+        version: '2',
+      },
+    ],
+  },
+  '1996-08': {
+    versions: [
+      {
+        day: 13,
+        version: '3',
+      },
+    ],
+  },
+  '1997-09': {
+    versions: [
+      {
+        day: 22,
+        version: '4',
+      },
+    ],
+  },
+  '1999-03': {
+    versions: [
+      {
+        day: 18,
+        version: '5',
+      },
+    ],
+  },
+  '2001-08': {
+    versions: [
+      {
+        day: 24,
+        version: '6',
+      },
+    ],
+  },
+  '2006-10': {
+    versions: [
+      {
+        day: 18,
+        version: '7',
+      },
+    ],
+  },
+  '2009-03': {
+    versions: [
+      {
+        day: 19,
+        version: '8',
+      },
+    ],
+  },
+  '2011-03': {
+    versions: [
+      {
+        day: 14,
+        version: '9',
+      },
+    ],
+  },
+  '2012-10': {
+    versions: [
+      {
+        day: 26,
+        version: '10',
+      },
+    ],
+  },
+  '2013-10': {
+    versions: [
+      {
+        day: 17,
+        version: '11',
+      },
+    ],
+  },
+};
+
+export default data;
