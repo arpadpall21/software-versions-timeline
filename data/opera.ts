206a245fcc16093ba875b378ca612f8a4bd23219
arpadpall21 <a.pall21@yahoo.fr>
2025-07-26 10:54:24 +0200

    opera: data file refactored

---
+++ data/opera.ts
@@ -0,0 +1,906 @@
+import { type RawHistoryData } from '@/misc/types';
+
+const data: RawHistoryData = {
+  '1995-01': {
+    versions: [
+      {
+        day: 1,
+        version: '1',
+      },
+    ],
+  },
+  '1996-01': {
+    versions: [
+      {
+        day: 1,
+        version: '3',
+      },
+    ],
+  },
+  '1996-12': {
+    versions: [
+      {
+        day: 31,
+        version: '3',
+      },
+    ],
+  },
+  '2000-06': {
+    versions: [
+      {
+        day: 28,
+        version: '4',
+      },
+    ],
+  },
+  '2000-12': {
+    versions: [
+      {
+        day: 6,
+        version: '5',
+      },
+    ],
+  },
+  '2001-11': {
+    versions: [
+      {
+        day: 29,
+        version: '6',
+      },
+    ],
+  },
+  '2003-01': {
+    versions: [
+      {
+        day: 28,
+        version: '7',
+      },
+    ],
+  },
+  '2005-04': {
+    versions: [
+      {
+        day: 19,
+        version: '8',
+      },
+    ],
+  },
+  '2006-06': {
+    versions: [
+      {
+        day: 20,
+        version: '9',
+      },
+    ],
+  },
+  '2009-09': {
+    versions: [
+      {
+        day: 1,
+        version: '10',
+      },
+    ],
+  },
+  '2011-03': {
+    versions: [
+      {
+        day: 18,
+        version: '11.11',
+      },
+    ],
+  },
+  '2012-06': {
+    versions: [
+      {
+        day: 14,
+        version: '12',
+      },
+    ],
+  },
+  '2013-07': {
+    versions: [
+      {
+        day: 2,
+        version: '15.0',
+      },
+    ],
+  },
+  '2013-08': {
+    versions: [
+      {
+        day: 27,
+        version: '16',
+      },
+    ],
+  },
+  '2013-10': {
+    versions: [
+      {
+        day: 8,
+        version: '17',
+      },
+    ],
+  },
+  '2013-11': {
+    versions: [
+      {
+        day: 19,
+        version: '18',
+      },
+    ],
+  },
+  '2014-01': {
+    versions: [
+      {
+        day: 28,
+        version: '19',
+      },
+    ],
+  },
+  '2014-03': {
+    versions: [
+      {
+        day: 4,
+        version: '20',
+      },
+    ],
+  },
+  '2014-05': {
+    versions: [
+      {
+        day: 6,
+        version: '21',
+      },
+    ],
+  },
+  '2014-06': {
+    versions: [
+      {
+        day: 3,
+        version: '22',
+      },
+    ],
+  },
+  '2014-07': {
+    versions: [
+      {
+        day: 22,
+        version: '23',
+      },
+    ],
+  },
+  '2014-09': {
+    versions: [
+      {
+        day: 2,
+        version: '24',
+      },
+    ],
+  },
+  '2014-10': {
+    versions: [
+      {
+        day: 15,
+        version: '25',
+      },
+    ],
+  },
+  '2014-12': {
+    versions: [
+      {
+        day: 13,
+        version: '26',
+      },
+    ],
+  },
+  '2015-01': {
+    versions: [
+      {
+        day: 27,
+        version: '27',
+      },
+    ],
+  },
+  '2015-03': {
+    versions: [
+      {
+        day: 10,
+        version: '28',
+      },
+    ],
+  },
+  '2015-04': {
+    versions: [
+      {
+        day: 28,
+        version: '29',
+      },
+    ],
+  },
+  '2015-06': {
+    versions: [
+      {
+        day: 9,
+        version: '30',
+      },
+    ],
+  },
+  '2015-08': {
+    versions: [
+      {
+        day: 4,
+        version: '31',
+      },
+    ],
+  },
+  '2015-09': {
+    versions: [
+      {
+        day: 15,
+        version: '32',
+      },
+    ],
+  },
+  '2015-10': {
+    versions: [
+      {
+        day: 27,
+        version: '33',
+      },
+    ],
+  },
+  '2015-12': {
+    versions: [
+      {
+        day: 8,
+        version: '34',
+      },
+    ],
+  },
+  '2016-02': {
+    versions: [
+      {
+        day: 2,
+        version: '35',
+      },
+    ],
+  },
+  '2016-03': {
+    versions: [
+      {
+        day: 15,
+        version: '36',
+      },
+    ],
+  },
+  '2016-05': {
+    versions: [
+      {
+        day: 4,
+        version: '37',
+      },
+    ],
+  },
+  '2016-06': {
+    versions: [
+      {
+        day: 8,
+        version: '38',
+      },
+    ],
+  },
+  '2016-08': {
+    versions: [
+      {
+        day: 2,
+        version: '39',
+      },
+    ],
+  },
+  '2016-09': {
+    versions: [
+      {
+        day: 20,
+        version: '40',
+      },
+    ],
+  },
+  '2016-10': {
+    versions: [
+      {
+        day: 25,
+        version: '41',
+      },
+    ],
+  },
+  '2016-12': {
+    versions: [
+      {
+        day: 13,
+        version: '42',
+      },
+    ],
+  },
+  '2017-02': {
+    versions: [
+      {
+        day: 7,
+        version: '43',
+      },
+    ],
+  },
+  '2017-05': {
+    versions: [
+      {
+        day: 10,
+        version: '45',
+      },
+    ],
+  },
+  '2017-06': {
+    versions: [
+      {
+        day: 22,
+        version: '46',
+      },
+    ],
+  },
+  '2017-08': {
+    versions: [
+      {
+        day: 9,
+        version: '47',
+      },
+    ],
+  },
+  '2017-09': {
+    versions: [
+      {
+        day: 27,
+        version: '48',
+      },
+    ],
+  },
+  '2017-11': {
+    versions: [
+      {
+        day: 8,
+        version: '49',
+      },
+    ],
+  },
+  '2018-01': {
+    versions: [
+      {
+        day: 4,
+        version: '50',
+      },
+    ],
+  },
+  '2018-02': {
+    versions: [
+      {
+        day: 7,
+        version: '51',
+      },
+    ],
+  },
+  '2018-04': {
+    versions: [
+      {
+        day: 22,
+        version: '52',
+      },
+    ],
+  },
+  '2018-05': {
+    versions: [
+      {
+        day: 10,
+        version: '53',
+      },
+    ],
+  },
+  '2018-07': {
+    versions: [
+      {
+        day: 28,
+        version: '54',
+      },
+    ],
+  },
+  '2018-08': {
+    versions: [
+      {
+        day: 16,
+        version: '55',
+      },
+    ],
+  },
+  '2018-09': {
+    versions: [
+      {
+        day: 25,
+        version: '56',
+      },
+    ],
+  },
+  '2018-11': {
+    versions: [
+      {
+        day: 28,
+        version: '57',
+      },
+    ],
+  },
+  '2019-01': {
+    versions: [
+      {
+        day: 23,
+        version: '58',
+      },
+    ],
+  },
+  '2019-04': {
+    versions: [
+      {
+        day: 9,
+        version: '60',
+      },
+    ],
+  },
+  '2019-06': {
+    versions: [
+      {
+        day: 27,
+        version: '62',
+      },
+    ],
+  },
+  '2019-08': {
+    versions: [
+      {
+        day: 20,
+        version: '63',
+      },
+    ],
+  },
+  '2019-10': {
+    versions: [
+      {
+        day: 8,
+        version: '64',
+      },
+    ],
+  },
+  '2019-11': {
+    versions: [
+      {
+        day: 14,
+        version: '65',
+      },
+    ],
+  },
+  '2020-01': {
+    versions: [
+      {
+        day: 8,
+        version: '66',
+      },
+    ],
+  },
+  '2020-02': {
+    versions: [
+      {
+        day: 25,
+        version: '67',
+      },
+    ],
+  },
+  '2020-04': {
+    versions: [
+      {
+        day: 22,
+        version: '68',
+      },
+    ],
+  },
+  '2020-06': {
+    versions: [
+      {
+        day: 24,
+        version: '69',
+      },
+    ],
+  },
+  '2020-07': {
+    versions: [
+      {
+        day: 28,
+        version: '70',
+      },
+    ],
+  },
+  '2020-09': {
+    versions: [
+      {
+        day: 15,
+        version: '71',
+      },
+    ],
+  },
+  '2020-10': {
+    versions: [
+      {
+        day: 21,
+        version: '72',
+      },
+    ],
+  },
+  '2020-12': {
+    versions: [
+      {
+        day: 9,
+        version: '73',
+      },
+    ],
+  },
+  '2021-02': {
+    versions: [
+      {
+        day: 2,
+        version: '74',
+      },
+    ],
+  },
+  '2021-03': {
+    versions: [
+      {
+        day: 24,
+        version: '75',
+      },
+    ],
+  },
+  '2021-04': {
+    versions: [
+      {
+        day: 28,
+        version: '76',
+      },
+    ],
+  },
+  '2021-06': {
+    versions: [
+      {
+        day: 24,
+        version: '77',
+      },
+    ],
+  },
+  '2021-08': {
+    versions: [
+      {
+        day: 3,
+        version: '78',
+      },
+    ],
+  },
+  '2021-09': {
+    versions: [
+      {
+        day: 14,
+        version: '79',
+      },
+    ],
+  },
+  '2021-10': {
+    versions: [
+      {
+        day: 5,
+        version: '80',
+      },
+    ],
+  },
+  '2021-11': {
+    versions: [
+      {
+        day: 4,
+        version: '81',
+      },
+    ],
+  },
+  '2021-12': {
+    versions: [
+      {
+        day: 2,
+        version: '82',
+      },
+    ],
+  },
+  '2022-01': {
+    versions: [
+      {
+        day: 19,
+        version: '83',
+      },
+    ],
+  },
+  '2022-02': {
+    versions: [
+      {
+        day: 16,
+        version: '84',
+      },
+    ],
+  },
+  '2022-03': {
+    versions: [
+      {
+        day: 23,
+        version: '85',
+      },
+    ],
+  },
+  '2022-04': {
+    versions: [
+      {
+        day: 20,
+        version: '86',
+      },
+    ],
+  },
+  '2022-05': {
+    versions: [
+      {
+        day: 17,
+        version: '87',
+      },
+    ],
+  },
+  '2022-06': {
+    versions: [
+      {
+        day: 8,
+        version: '88',
+      },
+    ],
+  },
+  '2022-07': {
+    versions: [
+      {
+        day: 7,
+        version: '89',
+      },
+    ],
+  },
+  '2022-08': {
+    versions: [
+      {
+        day: 18,
+        version: '90',
+      },
+    ],
+  },
+  '2022-09': {
+    versions: [
+      {
+        day: 14,
+        version: '91',
+      },
+    ],
+  },
+  '2022-10': {
+    versions: [
+      {
+        day: 19,
+        version: '92',
+      },
+    ],
+  },
+  '2022-11': {
+    versions: [
+      {
+        day: 17,
+        version: '93',
+      },
+    ],
+  },
+  '2022-12': {
+    versions: [
+      {
+        day: 15,
+        version: '94',
+      },
+    ],
+  },
+  '2023-02': {
+    versions: [
+      {
+        day: 1,
+        version: '95',
+      },
+      {
+        day: 22,
+        version: '96',
+      },
+    ],
+  },
+  '2023-03': {
+    versions: [
+      {
+        day: 22,
+        version: '97',
+      },
+    ],
+  },
+  '2023-04': {
+    versions: [
+      {
+        day: 20,
+        version: '98',
+      },
+    ],
+  },
+  '2023-05': {
+    versions: [
+      {
+        day: 16,
+        version: '99',
+      },
+    ],
+  },
+  '2023-06': {
+    versions: [
+      {
+        day: 20,
+        version: '100',
+      },
+    ],
+  },
+  '2023-07': {
+    versions: [
+      {
+        day: 26,
+        version: '101',
+      },
+    ],
+  },
+  '2023-08': {
+    versions: [
+      {
+        day: 23,
+        version: '102',
+      },
+    ],
+  },
+  '2023-10': {
+    versions: [
+      {
+        day: 3,
+        version: '103',
+      },
+      {
+        day: 23,
+        version: '104',
+      },
+    ],
+  },
+  '2023-11': {
+    versions: [
+      {
+        day: 14,
+        version: '105',
+      },
+    ],
+  },
+  '2023-12': {
+    versions: [
+      {
+        day: 19,
+        version: '106',
+      },
+    ],
+  },
+  '2024-02': {
+    versions: [
+      {
+        day: 7,
+        version: '107',
+      },
+    ],
+  },
+  '2024-03': {
+    versions: [
+      {
+        day: 5,
+        version: '108',
+      },
+      {
+        day: 27,
+        version: '109 ',
+      },
+    ],
+  },
+  '2024-05': {
+    versions: [
+      {
+        day: 14,
+        version: '110',
+      },
+    ],
+  },
+  '2024-06': {
+    versions: [
+      {
+        day: 12,
+        version: '111',
+      },
+    ],
+  },
+  '2024-07': {
+    versions: [
+      {
+        day: 11,
+        version: '112',
+      },
+    ],
+  },
+  '2024-08': {
+    versions: [
+      {
+        day: 22,
+        version: '113',
+      },
+    ],
+  },
+  '2024-09': {
+    versions: [
+      {
+        day: 25,
+        version: '114',
+      },
+    ],
+  },
+  '2024-11': {
+    versions: [
+      {
+        day: 27,
+        version: '115',
+      },
+    ],
+  },
+  '2025-01': {
+    versions: [
+      {
+        day: 8,
+        version: '116',
+      },
+    ],
+  },
+  '2025-02': {
+    versions: [
+      {
+        day: 13,
+        version: '117',
+      },
+    ],
+  },
+  '2025-04': {
+    versions: [
+      {
+        day: 14,
+        version: '118',
+      },
+    ],
+  },
+  '2025-05': {
+    versions: [
+      {
+        day: 12,
+        version: '119',
+      },
+    ],
+  },
+};
+
+export default data;
