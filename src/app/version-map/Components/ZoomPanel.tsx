42593cd8343d70df4a131ab3d9fc4810a95b6606
arpadpall21 <a.pall21@yahoo.fr>
2025-08-02 17:56:40 +0200

    zoom reset icon (reset)

--- src/app/version-map/Components/ZoomPanel.tsx
+++ src/app/version-map/Components/ZoomPanel.tsx
@@ -118,11 +118,25 @@ const ZoomPanel: React.FC<Props> = ({ ref, scrollZoomEnabled, setScrollZoomEnabl
           +
         </button>
         <button
-          className={'hover:bg-btnBgHov dark:hover:bg-btnBgHovD'}
+          className={'fill-btnFg dark:fill-btnFgD hover:bg-btnBgHov dark:hover:bg-btnBgHovD p-[7px]'}
           onClick={() => handleZoomChange('reset')}
           title={t('reset')}
         >
-          ↺
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="18px"
+            width="18px"
+            viewBox="0 -960 960 960"
+            stroke={'#4444bc'}
+            strokeWidth="10"
+          >
+            <path
+              d={`M480-80q-75 0-140.5-28.5t-114-77q-48.5-48.5-77-114T120-440h80q0 117 81.5 198.5T480
+                -160q117 0 198.5-81.5T760-440q0-117-81.5-198.5T480-720h-6l62 62-56 58-160-160 160-160 56 58
+                -62 62h6q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-440q0 75-28.5 140.5t-77 114q-48.5 48.5
+                -114 77T480-80Z`}
+            />
+          </svg>
         </button>
         <button
           className={'rounded-b-[0.25rem] hover:bg-btnBgHov dark:hover:bg-btnBgHovD'}
