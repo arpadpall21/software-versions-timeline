a98b5485aacb68a4520ba9f0bacff8f647972e50
arpadpall21 <a.pall21@yahoo.fr>
2025-08-02 18:18:52 +0200

    scroll zoom icon (dark click)

--- src/app/version-map/Components/ZoomPanel.tsx
+++ src/app/version-map/Components/ZoomPanel.tsx
@@ -100,10 +100,20 @@ const ZoomPanel: React.FC<Props> = ({ ref, scrollZoomEnabled, setScrollZoomEnabl
           flex flex-col mb-3 font-semibold text-xl`}
       >
         <button
+          className={`${scrollZoomEnabled ? 'fill-btnFgWarn dark:fill-btnFgWarnD' : 'fill-btnFg dark:fill-btnFgD'}
+            p-[5px]`}
           onMouseDown={() => setScrollZoomEnabled(!scrollZoomEnabled)}
           title={scrollZoomEnabled ? t('scrollZoomEnabled') : t('scrollZoomDisabled')}
         >
-          ⎋
+          <svg xmlns="http://www.w3.org/2000/svg" height="22px" width="22px" viewBox="0 -960 960 960">
+            <path
+              d={`M480-240q-97 0-166-66t-74-162l84 25q13 54 56 88.5T480-320q66 0 113-47t47-113q0-57-34.5-100T517-636l
+                -25-84q96 5 162 74t66 166q0 100-70 170t-170 70Zm0 160q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480v
+                -18q0-9 2-18l78 24v12q0 134 93 227t227 93q134 0 227-93t93-227q0-134-93-227t-227-93h-12l-24-78q9-2 18
+                -2h18q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm-59
+                -380L250-631l-50 151L80-880l400 120-151 50 171 171-79 79Z`}
+            />
+          </svg>
         </button>
       </div>
       <div
@@ -118,14 +128,14 @@ const ZoomPanel: React.FC<Props> = ({ ref, scrollZoomEnabled, setScrollZoomEnabl
           +
         </button>
         <button
-          className={'fill-btnFg dark:fill-btnFgD hover:bg-btnBgHov dark:hover:bg-btnBgHovD p-[7px]'}
+          className={'fill-btnFg dark:fill-btnFgD hover:bg-btnBgHov dark:hover:bg-btnBgHovD p-[8px]'}
           onClick={() => handleZoomChange('reset')}
           title={t('reset')}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="18px"
-            width="18px"
+            height="16px"
+            width="16px"
             viewBox="0 -960 960 960"
             stroke={'#4444bc'}
             strokeWidth="10"
