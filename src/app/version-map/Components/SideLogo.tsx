8a39f37084eaad077c29649169000f9e43d138bf
arpadpall21 <a.pall21@yahoo.fr>
2025-08-02 17:20:07 +0200

    remove timeline icon (bin)

--- src/app/version-map/Components/SideLogo.tsx
+++ src/app/version-map/Components/SideLogo.tsx
@@ -81,7 +81,14 @@ const SideLogo: React.FC<Props> = ({ twStyle, software, idx }) => {
             title={tSideLogo('removeTimeline')}
             onClick={handleBinIconClick}
           >
-            <p className={'m-auto'}> 🗑 </p>
+            <p className={'m-auto fill-gray-500 dark:fill-[#adadad]'}>
+              <svg xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" viewBox="0 -960 960 960">
+                <path
+                  d={`M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680
+                  -120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z`}
+                />
+              </svg>
+            </p>
           </div>
           <div className={`absolute btn dark:btnD h-[28px] w-[20px] z-10 right-2 bottom-2`}>
             <div className={`relative w-full h-full`}>
