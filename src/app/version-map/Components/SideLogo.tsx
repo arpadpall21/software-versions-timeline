033b154d91b0530f3ea07a3f9ef6bbda66193daa
Páll Árpád <a.pall21@yahoo.fr>
2025-08-04 08:10:37 +0200

    Use SVG icons
    * remove timeline icon (bin)
    
    * side logo dropdown button icon (arrow down)
    
    * zoom reset icon (reset)
    
    * scroll zoom icon (dark click)
    
    * replace reset zoom icon

--- src/app/version-map/Components/SideLogo.tsx
+++ src/app/version-map/Components/SideLogo.tsx
@@ -81,7 +81,15 @@ const SideLogo: React.FC<Props> = ({ twStyle, software, idx }) => {
             title={tSideLogo('removeTimeline')}
             onClick={handleBinIconClick}
           >
-            <p className={'m-auto'}> 🗑 </p>
+            <div className={'m-auto fill-gray-500 dark:fill-[#adadad]'}>
+              <svg xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" viewBox="0 -960 960 960">
+                <path
+                  d={`M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680
+                    -120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520
+                    -520Z`}
+                />
+              </svg>
+            </div>
           </div>
           <div className={`absolute btn dark:btnD h-[28px] w-[20px] z-10 right-2 bottom-2`}>
             <div className={`relative w-full h-full`}>
@@ -102,7 +110,11 @@ const SideLogo: React.FC<Props> = ({ twStyle, software, idx }) => {
                   peer-hover:bg-btnBgHov peer-hover:dark:bg-btnBgHovD`}
                 tabIndex={0}
               >
-                <span className={'m-auto text-xs'}> V </span>
+                <span className={'m-auto text-xs fill-btnFg dark:fill-btnFgD'}>
+                  <svg xmlns="http://www.w3.org/2000/svg" height="16px" width="16px" viewBox="0 -960 960 960">
+                    <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+                  </svg>
+                </span>
               </div>
             </div>
           </div>
--- src/app/version-map/Components/ZoomPanel.tsx
+++ src/app/version-map/Components/ZoomPanel.tsx
@@ -100,10 +100,20 @@ const ZoomPanel: React.FC<Props> = ({ ref, scrollZoomEnabled, setScrollZoomEnabl
           flex flex-col mb-3 font-semibold text-xl`}
       >
         <button
+          className={`${scrollZoomEnabled ? 'fill-btnFgWarn dark:fill-btnFgWarnD' : 'fill-btnFg dark:fill-btnFgD'}
+            p-[5px]`}
           onMouseDown={() => setScrollZoomEnabled(!scrollZoomEnabled)}
           title={scrollZoomEnabled ? t('scrollZoomEnabled') : t('scrollZoomDisabled')}
         >
-          ⎋
+          <svg xmlns="http://www.w3.org/2000/svg" height="22px" width="22px" viewBox="0 -960 960 960">
+            <path
+              d={`M480-240q-97 0-166-66t-74-162l84 25q13 54 56 88.5T480-320q66 0 113-47t47-113q0-57-34.5-100T517-636l
+                -25-84q96 5 162 74t66 166q0 100-70 170t-170 70Zm0 160q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480v
+                -18q0-9 2-18l78 24v12q0 134 93 227t227 93q134 0 227-93t93-227q0-134-93-227t-227-93h-12l-24-78q9-2 18
+                -2h18q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm-59
+                -380L250-631l-50 151L80-880l400 120-151 50 171 171-79 79Z`}
+            />
+          </svg>
         </button>
       </div>
       <div
@@ -118,11 +128,17 @@ const ZoomPanel: React.FC<Props> = ({ ref, scrollZoomEnabled, setScrollZoomEnabl
           +
         </button>
         <button
-          className={'hover:bg-btnBgHov dark:hover:bg-btnBgHovD'}
+          className={'fill-btnFg dark:fill-btnFgD hover:bg-btnBgHov dark:hover:bg-btnBgHovD p-[6px]'}
           onClick={() => handleZoomChange('reset')}
           title={t('reset')}
         >
-          ↺
+          <svg xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" viewBox="0 -960 960 960">
+            <path
+              d={`M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q
+                -32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t
+                -196 67Z`}
+            />
+          </svg>
         </button>
         <button
           className={'rounded-b-[0.25rem] hover:bg-btnBgHov dark:hover:bg-btnBgHovD'}
