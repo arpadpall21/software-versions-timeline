56326e6151a43d4061a24cb516b402712260a101
arpadpall21 <a.pall21@yahoo.fr>
2025-08-05 08:39:54 +0200

    add to software list

---
+++ data/mySql.ts
@@ -0,0 +1,14 @@
+import { type RawHistoryData } from '@/misc/types';
+
+const data: RawHistoryData = {
+  '1995-08': {
+    versions: [
+      {
+        day: 24,
+        version: '1',
+      },
+    ],
+  },
+};
+
+export default data;
