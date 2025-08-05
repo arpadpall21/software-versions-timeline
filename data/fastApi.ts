b16d5664cfbd82ada0472019f6aac491af19dc42
arpadpall21 <a.pall21@yahoo.fr>
2025-08-05 18:18:18 +0200

    add data file

---
+++ data/fastApi.ts
@@ -0,0 +1,118 @@
+import { type RawHistoryData } from '@/misc/types';
+
+const data: RawHistoryData = {
+  '2019-02': {
+    versions: [
+      {
+        day: 1,
+        version: '0.1',
+      },
+      {
+        day: 8,
+        version: '0.2',
+      },
+      {
+        day: 12,
+        version: '0.3',
+      },
+      {
+        day: 14,
+        version: '0.4',
+      },
+      {
+        day: 16,
+        version: '0.5',
+      },
+      {
+        day: 19,
+        version: '0.6',
+      },
+    ],
+  },
+  '2019-03': {
+    versions: [
+      {
+        day: 3,
+        version: '0.7',
+      },
+      {
+        day: 16,
+        version: '0.8',
+      },
+      {
+        day: 21,
+        version: '0.9',
+      },
+      {
+        day: 24,
+        version: '0.10',
+      },
+    ],
+  },
+  '2019-04': {
+    versions: [
+      {
+        day: 3,
+        version: '0.11',
+      },
+      {
+        day: 5,
+        version: '0.12',
+      },
+      {
+        day: 9,
+        version: '0.13',
+      },
+      {
+        day: 12,
+        version: '0.14',
+      },
+      {
+        day: 14,
+        version: '0.15',
+      },
+      {
+        day: 16,
+        version: '0.16',
+      },
+      {
+        day: 20,
+        version: '0.17',
+      },
+      {
+        day: 21,
+        version: '0.18',
+      },
+      {
+        day: 26,
+        version: '0.19',
+      },
+      {
+        day: 27,
+        version: '0.20',
+      },
+    ],
+  },
+  '2019-05': {
+    versions: [
+      {
+        day: 15,
+        version: '0.21',
+      },
+    ],
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+export default data;
