8f36a6514d7abc0e4b7463af22c6c076b81f08b9
arpadpall21 <a.pall21@yahoo.fr>
2025-08-05 09:01:41 +0200

    data

--- data/mySql.ts
+++ data/mySql.ts
@@ -1,11 +1,179 @@
 import { type RawHistoryData } from '@/misc/types';
 
 const data: RawHistoryData = {
-  '1995-08': {
+  '1995-03': {
     versions: [
       {
-        day: 24,
+        day: 23,
         version: '1',
+      },
+    ],
+  },
+  '1996-12': {
+    versions: [
+      {
+        day: 15,
+        version: '3.19',
+      },
+    ],
+  },
+  '1997-01': {
+    versions: [
+      {
+        day: 15,
+        version: '3.20',
+      },
+    ],
+  },
+  '1998-01': {
+    versions: [
+      {
+        day: 15,
+        version: '3.21',
+      },
+    ],
+  },
+  '1998-08': {
+    versions: [
+      {
+        day: 15,
+        version: '3.22 (alpha, beta)',
+      },
+    ],
+  },
+  '2000-01': {
+    versions: [
+      {
+        day: 22,
+        version: '3.23',
+      },
+    ],
+  },
+  '2003-03': {
+    versions: [
+      {
+        day: 22,
+        version: '4.0',
+      },
+    ],
+  },
+  '2004-10': {
+    versions: [
+      {
+        day: 15,
+        version: '4.1',
+      },
+    ],
+  },
+  '2005-10': {
+    versions: [
+      {
+        day: 15,
+        version: '5.0',
+      },
+    ],
+  },
+  '2008-11': {
+    versions: [
+      {
+        day: 27,
+        version: '5.1',
+      },
+    ],
+  },
+  '2010-12': {
+    versions: [
+      {
+        day: 15,
+        version: '5.5',
+      },
+    ],
+  },
+  '2013-04': {
+    versions: [
+      {
+        day: 23,
+        version: '5.7',
+      },
+    ],
+  },
+  '2016-09': {
+    versions: [
+      {
+        day: 12,
+        version: '8.0',
+      },
+    ],
+  },
+  '2023-07': {
+    versions: [
+      {
+        day: 18,
+        version: '8.1',
+      },
+    ],
+  },
+  '2023-10': {
+    versions: [
+      {
+        day: 25,
+        version: '8.2',
+      },
+    ],
+  },
+  '2024-01': {
+    versions: [
+      {
+        day: 16,
+        version: '8.3',
+      },
+    ],
+  },
+  '2024-04': {
+    versions: [
+      {
+        day: 30,
+        version: '8.4',
+      },
+    ],
+  },
+  '2024-07': {
+    versions: [
+      {
+        day: 1,
+        version: '9.0',
+      },
+    ],
+  },
+  '2024-10': {
+    versions: [
+      {
+        day: 15,
+        version: '9.1',
+      },
+    ],
+  },
+  '2025-01': {
+    versions: [
+      {
+        day: 21,
+        version: '9.2',
+      },
+    ],
+  },
+  '2025-04': {
+    versions: [
+      {
+        day: 15,
+        version: '9.3',
+      },
+    ],
+  },
+  '2025-07': {
+    versions: [
+      {
+        day: 22,
+        version: '9.4',
       },
     ],
   },
