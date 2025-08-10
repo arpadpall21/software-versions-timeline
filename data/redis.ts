7b0d692ff92bec171b2374dafa18fc3d3a35e760
arpadpall21 <a.pall21@yahoo.fr>
2025-08-10 12:10:15 +0200

    data

---
+++ data/redis.ts
@@ -0,0 +1,70 @@
+import { type RawHistoryData } from '@/misc/types';
+
+const data: RawHistoryData = {
+  '2018-08': {
+    versions: [
+      {
+        day: 11,
+        version: '5.0',
+      },
+    ],
+  },
+  '2020-05': {
+    versions: [
+      {
+        day: 1,
+        version: '6.0',
+      },
+    ],
+  },
+  '2021-08': {
+    versions: [
+      {
+        day: 11,
+        version: '6.2',
+      },
+    ],
+  },
+  '2023-02': {
+    versions: [
+      {
+        day: 1,
+        version: '6.4',
+      },
+    ],
+  },
+  '2023-08': {
+    versions: [
+      {
+        day: 1,
+        version: '7.2',
+      },
+    ],
+  },
+  '2024-02': {
+    versions: [
+      {
+        day: 1,
+        version: '7.4',
+      },
+    ],
+  },
+  '2024-11': {
+    versions: [
+      {
+        day: 1,
+        version: '7.8',
+      },
+    ],
+  },
+  '2025-05': {
+    versions: [
+      {
+        day: 1,
+        version: '7.22',
+      },
+    ],
+  },
+};
+
+export default data;
